f314fbae68446babb328858dff7f5ecd02e47fe8
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-09-15 11:43:43 +0330

    menu

--- src/features/Sidebar/Sidebar.jsx
+++ src/features/Sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SidebarHeader from "./components/SidebarHeader";
+import menus from "../../data/menus";
+import Menus from "./components/Menus";
 
 function Sidebar() {
   return (
     <aside className="w-68 p-6  z-10 bg-white h-screen sticky top-0 border-l primary-border-color">
       <SidebarHeader />
+      <Menus menus={menus} />
     </aside>
   );
 }
