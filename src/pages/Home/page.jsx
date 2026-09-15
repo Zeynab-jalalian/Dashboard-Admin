a5770ce564dfbdc7e72ef39fbaa067361979f84e
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-09-15 12:41:35 +0330

    sectiontitle

--- src/pages/Home/page.jsx
+++ src/pages/Home/page.jsx
@@ -1,5 +1,30 @@
+import SectionTitle from "../../components/common/SectionTitle";
+
 function Home() {
-  return <div>Home</div>;
+  const CTAButton = () => {
+    const clickHandler = () => {
+      console.log("clicked");
+    };
+    return (
+      <button
+        onClick={clickHandler}
+        className="primary-bg px-4 py-2 text-sm rounded-md cursor-pointer hover:opacity-90 text-white"
+      >
+        ایجاد محصول
+      </button>
+    );
+  };
+  return (
+    <>
+      <SectionTitle title="داشبورد" Buttons={<CTAButton />} />
+      {/* <Summaries /> */}
+      <div className="">
+        {/* <DetailsCharts />
+        <ProductsTable />
+        <QuickOverview /> */}
+      </div>
+    </>
+  );
 }
 
 export default Home;
